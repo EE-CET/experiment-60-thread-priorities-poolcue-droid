38604def00d2e7d217280cbb0384ee0eb1108983
poolcue-droid <gaurigthampi@gmail.com>
2026-02-20 09:52:08 +0530

    added code

--- src/ThreadPriorityDemo.java
+++ src/ThreadPriorityDemo.java
@@ -1,21 +1,24 @@
 class PriorityThread extends Thread {
     public void run() {
-        // Empty run method, we just need the thread object to exist and have properties
+        // No execution needed
     }
 }
 
 public class ThreadPriorityDemo {
     public static void main(String[] args) {
-        // TODO: Create three threads (t1, t2, t3)
-        
-        // TODO: Set priorities
-        // t1.setPriority(Thread.MIN_PRIORITY);
-        // t2.setPriority(Thread.NORM_PRIORITY);
-        // t3.setPriority(Thread.MAX_PRIORITY);
-        
-        // TODO: Print priorities in the required format
-        // System.out.println("Thread 1 Priority: " + t1.getPriority());
-        // System.out.println("Thread 2 Priority: " + t2.getPriority());
-        // System.out.println("Thread 3 Priority: " + t3.getPriority());
+        // Create threads
+        PriorityThread t1 = new PriorityThread();
+        PriorityThread t2 = new PriorityThread();
+        PriorityThread t3 = new PriorityThread();
+
+        // Set priorities
+        t1.setPriority(Thread.MIN_PRIORITY);   // 1
+        t2.setPriority(Thread.NORM_PRIORITY);  // 5
+        t3.setPriority(Thread.MAX_PRIORITY);   // 10
+
+        // Print priorities (exact format required)
+        System.out.println("Thread 1 Priority: " + t1.getPriority());
+        System.out.println("Thread 2 Priority: " + t2.getPriority());
+        System.out.println("Thread 3 Priority: " + t3.getPriority());
     }
 }
